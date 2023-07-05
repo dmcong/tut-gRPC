d7c9665e7da2a1c28c6167155775b1f7ee646d83
minhcong <83624415+dmcong@users.noreply.github.com>
2023-07-05 12:17:08 +0700

    .

--- main.go
+++ main.go
@@ -1,14 +1,49 @@
 package main
 
 import (
+	"strconv"
+	"time"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
 
+
 	router.GET("/", func(c *gin.Context) {
 		c.String(200, "Hello")
+	})
+
+	router.GET("/sum", func(c *gin.Context) {
+		startTime := time.Now()
+
+		numStr := c.DefaultQuery("number", "0")
+		num, err := strconv.Atoi(numStr)
+		if err != nil {
+			c.String(400, "Invalid number")
+			return
+		}
+		sum := 0
+		for i := 1; i <= num; i++ {
+			sum += i
+		}
+		c.JSON(200, gin.H{"time":  time.Since(startTime), "data": strconv.Itoa(sum)})
+	})
+
+	router.GET("/sumv2", func(c *gin.Context) {
+		startTime := time.Now()
+
+		numStr := c.DefaultQuery("number", "0")
+		num, err := strconv.Atoi(numStr)
+		if err != nil {
+			c.JSON(400, gin.H{"error": "Invalid number"})
+			return
+		}
+
+		sum := parallelSum(1,num)
+
+		c.JSON(200, gin.H{"time":  time.Since(startTime), "data": strconv.Itoa(sum)})
 	})
 
 	router.POST("/", func(c *gin.Context) {
@@ -24,4 +59,41 @@ func main() {
 	})
 
 	router.Run(":8080")
+}
+
+
+
+func parallelSum(start, end int) int {
+	delta := (end - start + 1) / 20
+	if delta == 0 {
+		sum := 0
+		for i := start; i <= end; i++ {
+			sum += i
+		}
+		return sum
+	}
+
+	resultChan := make(chan int)
+	for i := start; i <= end; i += delta {
+		s := i
+		e := i + delta - 1
+		if e > end {
+			e = end
+		}
+
+		go func(s, e int) {
+			sum := 0
+			for i := s; i <= e; i++ {
+				sum += i
+			}
+			resultChan <- sum
+		}(s, e)
+	}
+
+	sum := 0
+	for i := start; i <= end; i += delta {
+		sum += <-resultChan
+	}
+
+	return sum
 }
